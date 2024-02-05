c95034475443e48b7748382648d1cc7c91d55cdf
Nilsson1 <userfrenils@gmail.com>
2024-02-05 11:02:16 +0100

    Added unit test project

---
+++ Selvvalgt_Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Selvvalgt.Models;
+
+namespace Selvvalgt_Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
