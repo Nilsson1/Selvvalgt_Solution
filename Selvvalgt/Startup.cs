49ac68296c36addea32a6c86c8e1c2c974d0f66f
Nilsson1 <userfrenils@gmail.com>
2024-02-25 20:33:43 +0100

    exception handler back

--- Selvvalgt/Startup.cs
+++ Selvvalgt/Startup.cs
@@ -60,9 +60,9 @@ namespace Selvvalgt
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             // Configure the HTTP request pipeline.
-            // app.UseExceptionHandler("/Home/Error");
+            app.UseExceptionHandler("/Home/Error");
             // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-            // app.UseHsts();
+            app.UseHsts();
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
